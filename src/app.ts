6c35f13534f841db8425a61ea57288fc1d653366
Matias <carrillomatias121@gmail.com>
2026-07-26 23:01:10 -0500

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import type { Request, Response } from 'express'
+import express from 'express'
+import cors, { type CorsOptions } from 'cors'
+
+const corsOptions: CorsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET','POST','PUT','PATCH','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    maxAge: 86400
+}
+
+const app = express()
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get('/ping',(_req: Request, res: Response) => {
+    try {
+        res.json({
+            message: 'pong'
+        })
+        return
+    } catch(error) {
+        res.status(500).json({
+            message: 'error interno del sistema. Vuelva a intentar más tarde.',
+            code: 500
+        })
+        return
+    }
+})
+
+export default app
