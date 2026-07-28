18b91f2f9707ec30132a027157b6cc92e88ca6a5
Matias <carrillomatias121@gmail.com>
2026-07-27 23:02:11 -0500

    feat: add the emails sending function to auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,14 @@
 import type { IUsuario, IUsuarioCrearDTO } from "../models/usuario.model.js";
 import type UsuarioRepository from "../repositories/usuario.repository.js";
+import type EmailService from "./email.service.js";
 
 import bcrypt from 'bcrypt'
 import crypto from 'node:crypto'
 
+
 export default class AuthService {
 
-    constructor(private usuarioRepository: UsuarioRepository){}
+    constructor(private usuarioRepository: UsuarioRepository, private emailService: EmailService){}
 
     public async registrarUsuarioLocal(usuario: IUsuarioCrearDTO): Promise<IUsuario> {
 
@@ -19,7 +21,7 @@ export default class AuthService {
         const tokenVerificacion = crypto.randomInt(100000, 999999).toString()
         const expiresAt = new Date(Date.now() + 15 * 60 * 1000)
 
-        return await this.usuarioRepository.createUsuarioLocal({
+        const usuarioCreated = await this.usuarioRepository.createUsuarioLocal({
             nombre: usuario.nombre,
             email: usuario.email,
             contrasenaHash,
@@ -27,6 +29,10 @@ export default class AuthService {
             tokenVerificacionExpiresAt: expiresAt,
             foto_perfil: usuario.foto_perfil
         })
+
+        await this.emailService.enviarEmailCodigoVerificacion(usuario.email, tokenVerificacion)
+
+        return usuarioCreated
     }
 
 }
