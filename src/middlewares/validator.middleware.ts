c36e1ed13e026a85deb9df0289104255984d15b2
Matias <carrillomatias121@gmail.com>
2026-07-29 20:52:57 -0500

    feat: add schema validation middleware

---
+++ src/middlewares/validator.middleware.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+import { ZodError, ZodObject } from "zod";
+
+export const schemaValidation = (schema: ZodObject) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+        schema.parse(req.body)
+
+        next()
+    } catch(error) {
+        if(error instanceof ZodError) {
+            res.status(400).json(error.issues.map(issue => ({message: issue.message})))
+            return
+        }
+
+        res.status(400).json({
+            message: 'campos inválidos'
+        })
+        return
+    }
+}
