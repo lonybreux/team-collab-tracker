495b99dfc21bde97abcd2a98df4750ab4971f50d
Matias <carrillomatias121@gmail.com>
2026-08-10 13:26:34 -0500

    feat(auth): integrate google auth endpoints and query validation

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import schemaValidation from "../middlewares/validator.middleware.js";
-import { loginSchema, registrarSchema, verificarEmailSchema } from "../validators/auth.validator.js";
+import { loginSchema, registrarSchema, verificarEmailSchema, googleCallbackSchema } from "../validators/auth.validator.js";
 import AuthController from "../controllers/auth.controller.js";
 import AuthService from "../services/auth.service.js";
 import EmailService from "../services/email.service.js";
@@ -15,5 +15,7 @@ const authController = new AuthController(authService)
 router.post('/register', schemaValidation(registrarSchema), authController.register)
 router.post('/verificar-email', schemaValidation(verificarEmailSchema), authController.verificarEmail)
 router.post('/login',schemaValidation(loginSchema), authController.login)
+router.get('/google', authController.google)
+router.get('/google/callback', schemaValidation(googleCallbackSchema, 'query'), authController.googleCallback)
 
 export default router
